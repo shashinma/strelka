32602c81db30314d5b5fad1bea80052736b4ebe9
Sara Kalupa <skalupa0827@gmail.com>
2025-09-09 14:02:17 -0500

    Adding signature call to scan_test_clamAV

--- src/python/strelka/tests/test_scan_clamav.py
+++ src/python/strelka/tests/test_scan_clamav.py
@@ -1,8 +1,32 @@
 from pathlib import Path
+import logging
+import subprocess
 from unittest import TestCase, mock
 
 from strelka.scanners.scan_clamav import ScanClamav as ScanUnderTest
 from strelka.tests import run_test_scan
+
+
+def retrieve_signatures():
+    """
+    Utility function that will retrieve the ClamAV signature DB when called. Currently this is set up to run
+    when initializing the ClamAV scanner test. Should take around 10-15 seconds to retrieve the signatures from the remote DB.
+    """
+    logging.info("Retrieving ClamAV signatures via freshclam.")
+    try:
+        # Run freshclam to get the newest database signature
+        with subprocess.Popen(
+            ["freshclam"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        ) as proc:
+            stdout, stderr = proc.communicate()
+
+        logging.info(stdout)
+
+    except Exception as e:
+        error_msg = f"Failed to download clam signatures: {e}"
+        logging.error(error_msg)
 
 
 def test_scan_clamav(mocker):
@@ -10,6 +34,7 @@ def test_scan_clamav(mocker):
     Pass: Sample event matches output of scanner.
     Failure: Unable to load file or sample event fails to match.
     """
+    retrieve_signatures()
     test_scan_event = {
         "Data read": "0.51 MB (ratio 1.06",
         "Data scanned": "0.54 MB",
